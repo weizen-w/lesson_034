31ee11ef9143d4aaa11ebd9643b35a0459e3decf
Wladimir <weizen.w@gmx.de>
2023-05-04 15:58:43 +0200

    feat: add class Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -12,4 +12,13 @@ public class Triangle {
 //  При провале любой из проверок нужно вывести на экран соответствующее сообщение
 //  или выкинуть исключение.
 
+  private final int side1;
+  private final int side2;
+  private final int side3;
+
+  public Triangle(int side1, int side2, int side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
 }
